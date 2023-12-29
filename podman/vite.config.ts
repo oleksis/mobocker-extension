3e22dbc5c44631d39bbf81af469e35d5e926f635
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-12-29 11:42:07 -0500

    Update package.json, Containerfile, metadata.json tsconfig, vite for a React app for Podman Desktop

--- podman/vite.config.ts
+++ podman/vite.config.ts
@@ -7,22 +7,12 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
-    sourcemap: 'inline',
-    target: 'esnext',
     outDir: "dist",
-    assetsDir: '.',
-    lib: {
-      entry: './extension.ts',
-      formats: ['cjs'],
-    },
     rollupOptions: {
       external: [
         '@podman-desktop/api',
         ...builtinModules.flatMap(p => [p, `node:${p}`]),
-      ],
-      output: {
-        entryFileNames: '[name].js',
-      },
+      ]
     },
   },
   server: {
