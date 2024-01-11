ed8610c562654c8bcfef794b4a9b312311f048e4
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2024-01-11 17:25:13 -0500

    Specify that the output should be in ES module format

--- podman/vite.config.ts
+++ podman/vite.config.ts
@@ -13,7 +13,10 @@ export default defineConfig({
       external: [
         '@podman-desktop/api',
         ...builtinModules.flatMap(p => [p, `node:${p}`]),
-      ]
+      ],
+      output: {
+        format: 'esm',
+      },
     },
   },
   server: {
